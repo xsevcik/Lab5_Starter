87240afbafeea118d6aef78ea7105cb4650fe7c1
Xavier Sevcik <5400808+xsevcik@users.noreply.github.com>
2021-10-21 16:13:04 -0700

    complete expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,66 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+var listChoice;
+var volumeSlider;
+var playButton;
+var imageElement;
+var soundElement;
+var volumeIcon;
+var playConfetti = false;
+const jsConfetti = new JSConfetti();
 
 function init() {
-  // TODO
+  listChoice = document.getElementById("horn-select");
+  listChoice.addEventListener('change', updateSelection);
+
+  volumeSlider = document.getElementsByName("volume");
+  volumeSlider[0].addEventListener('input', updateVolume);
+
+  playButton = document.querySelector("section > button");
+  playButton.addEventListener('click', playSound);
+
+  imageElement = document.querySelector("section > img");
+  soundElement = document.querySelector("section > audio");
+  volumeIcon = document.querySelector("div > img")
+}
+
+function updateSelection() {
+  switch(listChoice.value) {
+    case "air-horn":
+      imageElement.src = "assets/images/air-horn.svg";
+      soundElement.src = "assets/audio/air-horn.mp3";
+      playConfetti = false;
+      break;
+    case "car-horn":
+      imageElement.src = "assets/images/car-horn.svg";
+      soundElement.src = "assets/audio/car-horn.mp3";
+      playConfetti = false;
+      break;
+    case "party-horn":
+      imageElement.src = "assets/images/party-horn.svg";
+      soundElement.src = "assets/audio/party-horn.mp3";
+      playConfetti = true;
+      break;
+  }
+}
+
+function updateVolume() {
+  if (volumeSlider[0].value == 0) {
+    volumeIcon.src = "assets/icons/volume-level-0.svg";
+  } else if (volumeSlider[0].value < 33) {
+    volumeIcon.src = "assets/icons/volume-level-1.svg";
+  } else if (volumeSlider[0].value < 67) {
+    volumeIcon.src = "assets/icons/volume-level-2.svg";
+  } else {
+    volumeIcon.src = "assets/icons/volume-level-3.svg";
+  }
+  soundElement.volume = volumeSlider[0].value / 100;
+}
+
+function playSound() {
+  soundElement.play();
+  if (playConfetti) {
+    jsConfetti.addConfetti();
+  }
 }
